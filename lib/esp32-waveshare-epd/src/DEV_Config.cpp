d4832172df9e7166d01cae10c5811681d903ea24
JKBit <jonaskaz2009@outlook.de>
2025-03-23 09:45:09 +0100

    Got it working

--- lib/esp32-waveshare-epd/src/DEV_Config.cpp
+++ lib/esp32-waveshare-epd/src/DEV_Config.cpp
@@ -28,22 +28,28 @@
 #
 ******************************************************************************/
 #include "DEV_Config.h"
-#include "SPI.h"
-
-static SPIClass * display_spi = new SPIClass(FSPI);
 
 void GPIO_Config(void)
-{    
-    display_spi->begin(EPD_SCK_PIN, -1, EPD_MOSI_PIN);
-    display_spi->setFrequency(20000000);
-    //display_spi->beginTransaction(SPISettings(20000000, 1, 0));
-    pinMode(EPD_BUSY_PIN, INPUT);
-    pinMode(EPD_RST_PIN, OUTPUT);
-    pinMode(EPD_DC_PIN, OUTPUT);
+{
+    pinMode(EPD_BUSY_PIN,  INPUT);
+    pinMode(EPD_RST_PIN , OUTPUT);
+    pinMode(EPD_DC_PIN  , OUTPUT);
+    
+    pinMode(EPD_SCK_PIN, OUTPUT);
+    pinMode(EPD_MOSI_PIN, OUTPUT);
+    pinMode(EPD_CS_PIN , OUTPUT);
 
-    pinMode(EPD_CS_PIN, OUTPUT);
+    digitalWrite(EPD_CS_PIN , HIGH);
+    digitalWrite(EPD_SCK_PIN, LOW);
+}
 
-    digitalWrite(EPD_CS_PIN, HIGH);
+void GPIO_Mode(UWORD GPIO_Pin, UWORD Mode)
+{
+    if(Mode == 0) {
+        pinMode(GPIO_Pin , INPUT);
+	} else {
+		pinMode(GPIO_Pin , OUTPUT);
+	}
 }
 /******************************************************************************
 function:	Module Initialize, the BCM2835 library and initialize the pins, SPI protocol
@@ -52,36 +58,66 @@ Info:
 ******************************************************************************/
 UBYTE DEV_Module_Init(void)
 {
-    // gpio
-    GPIO_Config();
+	//gpio
+	GPIO_Config();
 
-    // serial printf
-    Serial.begin(115200);
+	//serial printf
+	Serial.begin(115200);
 
-    // spi
-    // SPI.setDataMode(SPI_MODE0);
-    // SPI.setBitOrder(MSBFIRST);
-    // SPI.setClockDivider(SPI_CLOCK_DIV4);
-    // SPI.begin();
+	// spi
+	// SPI.setDataMode(SPI_MODE0);
+	// SPI.setBitOrder(MSBFIRST);
+	// SPI.setClockDivider(SPI_CLOCK_DIV4);
+	// SPI.begin();
 
-    return 0;
+	return 0;
 }
 
 /******************************************************************************
 function:
-            SPI read and write
+			SPI read and write
 ******************************************************************************/
 void DEV_SPI_WriteByte(UBYTE data)
 {
-    // SPI.beginTransaction(spi_settings);
-    
-    //digitalWrite(EPD_CS_PIN, GPIO_PIN_RESET);
-    REG_WRITE(GPIO_OUT_W1TC_REG, 1 << EPD_CS_PIN);
+    //SPI.beginTransaction(spi_settings);
+    digitalWrite(EPD_CS_PIN, GPIO_PIN_RESET);
 
-    display_spi->transfer(data);
+    for (int i = 0; i < 8; i++)
+    {
+        if ((data & 0x80) == 0) digitalWrite(EPD_MOSI_PIN, GPIO_PIN_RESET); 
+        else                    digitalWrite(EPD_MOSI_PIN, GPIO_PIN_SET);
 
-    //digitalWrite(EPD_CS_PIN, GPIO_PIN_SET);
-    REG_WRITE(GPIO_OUT_W1TS_REG, 1 << EPD_CS_PIN);
+        data <<= 1;
+        digitalWrite(EPD_SCK_PIN, GPIO_PIN_SET);     
+        digitalWrite(EPD_SCK_PIN, GPIO_PIN_RESET);
+    }
 
-    // SPI.endTransaction();
+    //SPI.transfer(data);
+    digitalWrite(EPD_CS_PIN, GPIO_PIN_SET);
+    //SPI.endTransaction();	
+}
+
+UBYTE DEV_SPI_ReadByte()
+{
+    UBYTE j=0xff;
+    GPIO_Mode(EPD_MOSI_PIN, 0);
+    digitalWrite(EPD_CS_PIN, GPIO_PIN_RESET);
+    for (int i = 0; i < 8; i++)
+    {
+        j = j << 1;
+        if (digitalRead(EPD_MOSI_PIN))  j = j | 0x01;
+        else                            j = j & 0xfe;
+        
+        digitalWrite(EPD_SCK_PIN, GPIO_PIN_SET);     
+        digitalWrite(EPD_SCK_PIN, GPIO_PIN_RESET);
+    }
+    digitalWrite(EPD_CS_PIN, GPIO_PIN_SET);
+    GPIO_Mode(EPD_MOSI_PIN, 1);
+    return j;
+}
+
+void DEV_SPI_Write_nByte(UBYTE *pData, UDOUBLE len)
+{
+    for (int i = 0; i < len; i++)
+        DEV_SPI_WriteByte(pData[i]);
 }
--- lib/esp32-waveshare-epd/src/DEV_Config.h
+++ lib/esp32-waveshare-epd/src/DEV_Config.h
@@ -67,6 +67,9 @@
 
 /*------------------------------------------------------------------------------------------------------*/
 UBYTE DEV_Module_Init(void);
+void GPIO_Mode(UWORD GPIO_Pin, UWORD Mode);
 void DEV_SPI_WriteByte(UBYTE data);
+UBYTE DEV_SPI_ReadByte();
+void DEV_SPI_Write_nByte(UBYTE *pData, UDOUBLE len);
 
 #endif
